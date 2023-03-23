04a9c7d9059f515a863c4c58a258b8374594075e
GoptarEA <goptarea@gmail.com>
2023-03-23 22:43:59 +0300

    Сессия 1

--- main.py
+++ main.py
@@ -4,21 +4,25 @@ connect = psycopg2.connect(dbname="Users", user="postgres", password="admin", ho
 connect.autocommit = True
 cur = connect.cursor()
 
-cur.execute("""CREATE TABLE IF NOT EXISTS users (
+cur.execute("""CREATE TABLE IF NOT EXISTS uslugi (
     id serial PRIMARY KEY,
-    first_name varchar(50) NOT NULL,
-    last_name varchar(50) NOT NULL
+    code varchar(10) NOT NULL,
+    title varchar(100) NOT NULL,
+    price float(4) NOT NULL
 );
 """)
-# connect.commit()
-first = "Petya"
-second = "Vasiliev"
 
-cur.execute("INSERT INTO users (first_name, last_name) VALUES (%s, %s)", (first, second))
 
-with open('table1.txt') as table:
+
+
+with open('table.csv', encoding='utf-8-sig') as table:
     for row in table:
-        print(row)
+        sp = row.strip().split(";")
+        print(sp)
+        cur.execute(
+            "INSERT INTO uslugi (code, title, price) VALUES (%s, %s, %s)",
+            (sp[0], sp[1], float(sp[2].replace(',', '.')))
+        )
 
 cur.close()
 connect.close()
